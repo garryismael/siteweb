8ed52a585d0af92a18c0a17ad01e91361abf164a
garryismael <tahinjanaharygarry@gmail.com>
2022-05-13 17:13:45 +0300

    update

---
+++ src/layouts/admin/AdminLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header />
+    <Sidenav />
+  </div>
+</template>
+<script>
+import Header from "@/views/admin/Header.vue";
+import Sidenav from "@/views/admin/Sidenav.vue";
+
+export default {
+  components: {
+    Header,
+    Sidenav,
+  },
+};
+</script>
---
+++ src/views/auth/LoginRegister.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="body">
+    <div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true">
+			<div class="signup">
+				<form>
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input type="text" name="txt" placeholder="User name" class="input">
+					<input type="email" name="email" placeholder="Email" class="input">
+					<input type="password" name="pswd" placeholder="Password" class="input">
+					<button class="login-btn">Sign up</button>
+				</form>
+			</div>
+
+			<div class="login">
+				<form>
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" class="input">
+					<input type="password" name="pswd" placeholder="Password" class="input">
+					<button class="login-btn">Login</button>
+				</form>
+			</div>
+	</div>
+  </div>
+</template>
+<style scoped>
+@import "./style/style.css";
+</style>
